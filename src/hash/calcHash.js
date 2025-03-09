8b91027fd6f2c037f70a60ee4e3dc9e9a20318c2
SIVK1 <artems2715@gmail.com>
2025-03-09 20:39:35 +0300

    feat: hash complete

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+import { resolve } from 'node:path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = resolve('src/hash/files/fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+    
+    stream.pipe(hash);
+    
+    await new Promise((resolve, reject) => {
+        hash.on('finish', () => {
+            process.stdout.write(hash.digest('hex') + '\n');
+            resolve();
+        });
+        stream.on('error', reject);
+    });
 };
 
 await calculateHash();
